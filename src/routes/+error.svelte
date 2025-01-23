744f95101887aab322cc499e29ffc87067ca95b8
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2025-01-23 10:49:55 +0100

    Added 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import { page } from "$app/state";
+    import { DarkMode } from "flowbite-svelte";
+    import { TeddyBearSolid } from "flowbite-svelte-icons";
+</script>
+
+<div class="min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-900 text-gray-500 dark:text-gray-400 gap-20">
+    <div class="text-start gap-4">
+        <h1 class="text-4xl font-bold">{page.status}</h1>
+        <p class="text-lg">{page.error?.message}</p>
+    </div>
+    <div>
+        <TeddyBearSolid class="w-24 h-24 text-gray-500 dark:text-gray-400" />
+    </div>
+</div>
